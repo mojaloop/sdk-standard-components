bf3679a4d09f8ad8360cb853db750009e337cb15
Ramiro González Maciel <ramirogm@gmail.com>
2019-11-08 14:48:45 -0300

    Removing unnecesary Buffer import; ensuring Buffer is not converted into String

--- src/lib/mojaloop-requests/toString.js
+++ src/lib/mojaloop-requests/toString.js
@@ -1,9 +1,8 @@
-var Buffer = require('buffer').Buffer;
 
 module.exports = function toString(obj) {
-    if (typeof obj === 'string')
+    if (typeof obj === 'string' || Buffer.isBuffer(obj))
         return obj;
-    if (typeof obj === 'number' || Buffer.isBuffer(obj))
+    if (typeof obj === 'number')
         return obj.toString();
     return JSON.stringify(obj);
 };
