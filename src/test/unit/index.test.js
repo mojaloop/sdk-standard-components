d0b88b4a40c1d33c5baed248c5e4c4ea62383c97
Paweł Marzec <pm@eoln.eu>
2020-07-13 10:44:15 +0200

    fix main module layout

--- src/test/unit/index.test.js
+++ src/test/unit/index.test.js
@@ -5,28 +5,25 @@
  *  specified in the corresponding source code repository.                *
  *                                                                        *
  *  ORIGINAL AUTHOR:                                                      *
- *       James Bush - james.bush@modusbox.com                             *
+ *       pawel.marzec - pawel.marzec@modusbox.com                         *
  **************************************************************************/
 
-'use strict';
+ describe('index layout', () => {
+    const index = require('../../index');
+    const shouldBeExported = [
+      'Errors',
+      'Ilp',
+      'Jws',
+      'Logger',
+      'MojaloopRequests',
+      'ThirdpartyRequests',
+      'request',
+      'WSO2Auth',
+    ];
 
-
-const Errors = require('./lib/errors');
-const Ilp = require('./lib/ilp');
-const Jws = require('./lib/jws');
-const Logger = require('./lib/logger');
-const { MojaloopRequests, ThirdpartyRequests } = require('./lib/requests');
-const request = require('./lib/request');
-const WSO2Auth = require('./lib/WSO2Auth');
-
-
-module.exports = {
-    Errors,
-    Ilp,
-    Jws,
-    Logger,
-    MojaloopRequests,
-    ThirdpartyRequests,
-    request,
-    WSO2Auth,
-};
+    shouldBeExported.forEach((item) => {
+      it(`should export '${item}'`, () => {
+        expect(index[item]).toBeDefined();
+      });
+    })
+ })
