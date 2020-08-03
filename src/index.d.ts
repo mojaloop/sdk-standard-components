d0cfa7eed65ac3c069d55c7dc170cc5e6f40681d
Abhimanyu Kapur <38531241+akapur99@users.noreply.github.com>
2020-08-03 11:37:10 +0530

    Update Payload Type
    Allow it to be string or undefined in alignment with Auth Service internal consent model

--- src/index.d.ts
+++ src/index.d.ts
@@ -79,7 +79,7 @@ declare namespace SDKStandardComponents {
             payload: string;
             signature: string | null;
         },
-        payload: string | null;
+        payload: string | undefined;
     }
 
     type TCredentialScope = {
