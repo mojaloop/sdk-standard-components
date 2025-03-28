d5387d91658a61ac7b05dced3e93dfef472d455d
geka.evk <geka.evk@gmail.com>
2025-03-28 12:32:22 +0000

    fix(csi-1350): made http retry defaults configurable

--- src/lib/httpRequester/constants.js
+++ src/lib/httpRequester/constants.js
@@ -1,3 +1,32 @@
+/*****
+ License
+ --------------
+ Copyright © 2020-2025 Mojaloop Foundation
+ The Mojaloop files are made available by the Mojaloop Foundation under the Apache License, Version 2.0 (the "License") and you may not use these files except in compliance with the License. You may obtain a copy of the License at
+
+ http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, the Mojaloop files are distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
+
+ Contributors
+ --------------
+ This is the official list of the Mojaloop project contributors for this file.
+ Names of the original copyright holders (individuals or organizations)
+ should be listed with a '*' in the first column. People who have
+ contributed from an organization can be listed under the organization
+ that actually holds the copyright for their contributions (see the
+ Mojaloop Foundation for an example). Those individuals should have
+ their names indented and be marked with a '-'. Email address can be added
+ optionally within square brackets <email>.
+
+ * Mojaloop Foundation
+ * Eugen Klymniuk <eugen.klymniuk@infitx.com>
+
+ --------------
+ ******/
+
+const { env } = require('node:process');
+
 const ResponseType = Object.freeze({
     ArrayBuffer: 'arraybuffer',
     JSON:  'json',
@@ -6,10 +35,9 @@ const ResponseType = Object.freeze({
     // Document: 'document', // this available in axios
 });
 
-const DEFAULT_TIMEOUT = 65_000; // there's lots of callback/request timeouts in TTK with 60_000 ms
-const DEFAULT_RETRIES = 3;
-const DEFAULT_RETRY_DELAY = 10; // in ms
-// todo: add possibility to override these defaults with env vars
+const DEFAULT_TIMEOUT = env.HTTP_DEFAULT_TIMEOUT ? parseInt(env.HTTP_DEFAULT_TIMEOUT, 10) : 65_000; // there's lots of callback/request timeouts in TTK with 60_000 ms
+const DEFAULT_RETRIES = env.HTTP_DEFAULT_RETRIES ? parseInt(env.HTTP_DEFAULT_RETRIES, 10) : 3;
+const DEFAULT_RETRY_DELAY = env.HTTP_DEFAULT_RETRY_DELAY ? parseInt(env.HTTP_DEFAULT_RETRY_DELAY, 10) : 10; // in ms
 
 module.exports = {
     ResponseType,
